0faf9b311f444c4a977054343bf046175b219692
BernardoKava <bernardo.kava.bk@outlook.com>
2018-11-07 20:30:53 +0000

    testing deployment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_083053) do
+ActiveRecord::Schema.define(version: 2018_11_04_215907) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "address"
@@ -343,45 +343,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_083053) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.decimal "balance"
-    t.integer "ledger_id"
     t.string "saving_number"
-  end
-
-  create_table "searches", force: :cascade do |t|
-    t.string "keyword"
-    t.integer "address_id"
-    t.integer "budget_id"
-    t.integer "cashflow_id"
-    t.integer "comment_id"
-    t.integer "delivery_id"
-    t.integer "department_id"
-    t.integer "email_id"
-    t.integer "employment_id"
-    t.integer "employmentreference_id"
-    t.integer "expense_id"
-    t.integer "financialinstitution_id"
-    t.integer "income_id"
-    t.integer "inflow_id"
-    t.integer "inflowtype_id"
-    t.integer "inventory_id"
-    t.integer "item_id"
-    t.integer "legacy_ulsterbank_id"
-    t.integer "lodgement_id"
-    t.integer "outflow_id"
-    t.integer "outflowtype_id"
-    t.integer "passwordrepository_id"
-    t.integer "person_id"
-    t.integer "post_id"
-    t.integer "qualification_id"
-    t.integer "saving_id"
-    t.integer "telephone_id"
-    t.integer "user_id"
-    t.integer "withdrawal_id"
-    t.date "from_date"
-    t.date "to_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "ledger_id"
   end
 
   create_table "telephones", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -26,5 +26,117 @@ end
 puts "There are now #{LegacyUlsterbank.count} rows in the table"
 
 
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'outflowtypes.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Outflowtype.new
+  t.name = row['flowtype']
 
+  t.save
+  puts "#{t.name} saved"
+end
+puts "There are now #{Outflowtype.count} rows in the table"
+
+
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'budgettest.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Budget.new
+  t.year = row['year']
+  t.month = row['month']
+  t.budget_date = row['budget_date']
+  t.name = row['name']
+
+  t.save
+  puts "#{t.name} saved"
+end
+puts "There are now #{Budget.count} rows in the table"
+
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'budgettest.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Cashflow.new
+  t.year = row['year']
+  t.month = row['month']
+  t.accounting_date = row['budget_date']
+  t.name = row['name']
+
+  t.save
+  puts "#{t.name} saved"
+end
+puts "There are now #{Cashflow.count} rows in the table"
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'budgettest.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Saving.new
+  t.year = row['year']
+  t.month = row['month']
+  t.saving_date = row['budget_date']
+  t.name = row['name']
+
+  t.save
+  puts "#{t.name} saved"
+end
+puts "There are now #{Saving.count} rows in the table"
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'passwords.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Passwordrepository.new
+  t.system_name = row['system_name']
+  t.user_name= row['user_name']
+  t.system_password = row['system_password']
+  t.active = row['active']
+
+  t.save
+  puts "#{t.system_name} saved"
+end
+puts "There are now #{Passwordrepository.count} rows in the table"
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'people.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Person.new
+  t.name = row['name']
+  t.surname= row['surname']
+  t.dob = row['dob']
+  t.active = row['active']
+  t.fullname = row['fullname']
+
+  t.save
+  puts "#{t.name} saved"
+end
+puts "There are now #{Person.count} rows in the table"
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'departments.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Department.new
+  t.name = row['name']
+  t.description= row['description']
+  t.active = row['active']
+
+  t.save
+  puts "#{t.name} saved"
+end
+puts "There are now #{Department.count} rows in the table"
+
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'inflowtypes.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Inflowtype.new
+  t.name = row['inflow']
+  t.save
+  puts "#{t.name} saved"
+end
+puts "There are now #{Inflowtype.count} rows in the table"
 
