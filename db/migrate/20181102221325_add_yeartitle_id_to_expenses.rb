d55ac3769927abd3b6d0c25c2741e14c2776f712
BernardoKava <bernardo.kava.bk@outlook.com>
2018-11-07 12:44:47 +0000

    fixing migration issues

--- db/migrate/20181102221325_add_yeartitle_id_to_expenses.rb
+++
@@ -1,5 +0,0 @@
-class AddYeartitleIdToExpenses < ActiveRecord::Migration[5.2]
-  def change
-    add_column :expenses, :yeartitle_id, :integer
-  end
-end
--- db/migrate/20181102221354_add_monthtitle_id_to_expenses.rb
+++
@@ -1,5 +0,0 @@
-class AddMonthtitleIdToExpenses < ActiveRecord::Migration[5.2]
-  def change
-    add_column :expenses, :monthtitle_id, :integer
-  end
-end
