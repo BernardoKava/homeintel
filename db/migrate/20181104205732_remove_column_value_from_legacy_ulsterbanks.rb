36368ac93ff54d2b2b73cb63f7f7ebf6b7280e74
BernardoKava <bernardo.kava.bk@outlook.com>
2018-11-07 13:58:48 +0000

    fixing migration issues

--- db/migrate/20181104205732_remove_column_value_from_legacy_ulsterbanks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnValueFromLegacyUlsterbanks < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :legacy_ulsterbanks, :value, :numeric
-  end
-end
--- db/migrate/20181104205848_add_column_transaction_value_to_legacy_ulsterbanks.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnTransactionValueToLegacyUlsterbanks < ActiveRecord::Migration[5.2]
-  def change
-    add_column :legacy_ulsterbanks, :transaction_value, :numeric
-  end
-end
