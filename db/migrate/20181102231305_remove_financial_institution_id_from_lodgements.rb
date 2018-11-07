bb50f025db47f9f18da0a8906d84871b5b1a7579
BernardoKava <bernardo.kava.bk@outlook.com>
2018-11-07 12:54:16 +0000

    fixing migration issues

--- db/migrate/20181102231305_remove_financial_institution_id_from_lodgements.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFinancialInstitutionIdFromLodgements < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :lodgements, :financial_institution_id, :integer
-  end
-end
--- db/migrate/20181102231335_remove_financial_institution_id_from_withdrawals.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFinancialInstitutionIdFromWithdrawals < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :withdrawals, :financial_institution_id, :integer
-  end
-end
