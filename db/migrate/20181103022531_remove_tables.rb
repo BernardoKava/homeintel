785792b64abf0dbd3207ee311021cbb2897b090c
BernardoKava <bernardo.kava.bk@outlook.com>
2018-11-07 13:29:10 +0000

    fixing migration issues

--- db/migrate/20181103022531_remove_tables.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTables < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :year_titles
-    drop_table :month_titles
-    drop_table :outflow_types
-    drop_table :financial_institutions
-
-  end
-end
