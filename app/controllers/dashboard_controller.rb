2b9aa4648e1e6f8fe8eddfeea082e1b99d6e74ff
BernardoKava <bernardo.kava.bk@outlook.com>
2018-11-09 17:02:53 +0000

    added title to pages

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,48 +11,9 @@ class DashboardController < ApplicationController
 
   end
 
-  def incomes
-    @incomes     =  Income.all.paginate(page: params[:page], :per_page => 10).order("created_at DESC")
-  end
-
-  def expenses
-    @expenses    =  Expense.all.paginate(page: params[:page], :per_page => 10).order("created_at DESC")
-  end
-
-  def inflows
-    @incomes     =  Income.all.paginate(page: params[:page], :per_page => 10).order("created_at DESC")
-  end
-
-  def expense
-    @expenses    =  Expense.all.paginate(page: params[:page], :per_page => 10).order("created_at DESC")
-  end
-
-
-
-
-
-
-
 
   def passwords
     @passwords = Passwordrepository.where(:active => true).paginate(page: params[:page], :per_page => 4).order("created_at DESC")
   end
 
-  def legacy
-
-    @ulster_bank_transactions = if params[:term]
-                                  LegacyUlsterbank.where('description LIKE?', "%#{params[:term]}%")
-                                      .paginate(page: params[:page], :per_page => 10).order("created_at DESC")
-                                else
-                                  LegacyUlsterbank.all.paginate(page: params[:page], :per_page => 10).order("created_at DESC")
-                                end
-    @total = @ulster_bank_transactions.sum(:trans_value)
-    @records = @ulster_bank_transactions.total_entries
-  end
-
-
-  def task_params
-    params.require(:dashboard).permit(:term)
-
-  end
 end
